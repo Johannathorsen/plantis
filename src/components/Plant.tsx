d49b5b52273dc7e8ec381d8e6c017b75e33585ff
Johanna Thorsen <johanna@dumfan.net>
2023-02-02 03:14:21 +0100

    use github api for specific plant info

--- src/components/Plant.tsx
+++ src/components/Plant.tsx
@@ -7,15 +7,46 @@ import Paper from '@mui/material/Paper';
 import Stack from '@mui/material/Stack';
 import { Plant as PlantType } from './PlantGallery';
 
+type Image = {
+  name: string,
+  download_url: string,
+}
+
+type Treatment = {
+  events?: {
+    water?: boolean,
+    nutrition?: boolean,
+  },
+  text?: string
+};
+
+type Treatments = Record<string, Treatment>;
 
 export default function Plant() {
   const { id } = useParams();
   const [plant, setPlant ] = useState<PlantType>({});
-  // const [treatments, setTreatments ] = useState([{events: {water: false, nutrition: false}, text: ''}]);
+  const [treatments, setTreatments ] = useState<Treatments>({});
+  const [images, setImages] = useState<Record<string, string>>({})
 
   useEffect(() => {
     fetch(`https://raw.githubusercontent.com/Johannathorsen/plantis/main/data/${id}/plant.json?ref=main`).then((res)=>res.json()).then((data: PlantType)=> {
       setPlant(data);
+    });
+    }, [id] )
+
+  useEffect(() => {
+    fetch(`https://raw.githubusercontent.com/Johannathorsen/plantis/main/data/${id}/treatments.json?ref=main`).then((res)=>res.json()).then((data: Treatments)=> {
+      setTreatments(data);
+    });
+    }, [id] )
+
+  useEffect(() => {
+    fetch(`https://api.github.com/repos/johannathorsen/plantis/contents/data/${id}/images?ref=main`).then((res)=>res.json()).then((data)=> {
+      const images = {} as Record<string, string>;
+      data.forEach((image: Image) => {
+        images[image.name] = image.download_url
+      })
+      setImages(images);
     });
     }, [id] )
 
@@ -27,16 +58,21 @@ export default function Plant() {
       <Box sx={{ width: '100%' }}>
         <Stack spacing={2}>
           <h3 className="date">10 augusti 2022</h3>
-          <Paper className="item">
-          <img
-              src={`?w=248&fit=crop&auto=format`}
-              srcSet={`?w=248&fit=crop&auto=format&dpr=2 2x`}
-              alt={plant.name}
-              loading="lazy"
-            />
-          </Paper>
-          <Paper className="item">Item 2</Paper>
-          <Paper className="item">Item 3</Paper>
+          <Paper className="item">{JSON.stringify(treatments)}</Paper>
+          {
+            Object.entries(images).map(([id, url]) => {
+              return (
+              <Paper className="item">
+              <img
+                  src={`${url}?w=248&fit=crop&auto=format`}
+                  srcSet={`${url}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                  alt={id}
+                  loading="lazy"
+                />
+              </Paper>
+              )
+            })
+          }
         </Stack>
       </Box>
     </div>
